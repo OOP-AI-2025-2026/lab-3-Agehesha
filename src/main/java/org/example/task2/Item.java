7e6df7a71a224f7f61edbbf26283b2741b11b338
Agehesha <romashugaev99@gmail.com>
2025-09-23 19:21:30 +0300

    Update Item.java

--- src/main/java/org/example/task2/Item.java
+++ src/main/java/org/example/task2/Item.java
@@ -1,42 +1,38 @@
 package org.example.task2;
 
 public class Item {
-    private final long id;
-    private String name;
-    private double price;
+  private final long id;
+  private String name;
+  private double price;
 
-    public Item(long id, String name, double price) {
-        this.id = id;
-        this.name = name;
-        this.price = price < 0 ? 0.0 : price;
-    }
+  public Item(long id, String name, double price) {
+    this.id = id;
+    this.name = name;
+    this.price = price < 0 ? 0.0 : price;
+  }
 
-    public long getId() {
-        return this.id;
-    }
+  public long getId() {
+    return this.id;
+  }
 
-    public String getName() {
-        return this.name;
-    }
+  public String getName() {
+    return this.name;
+  }
 
-    public double getPrice() {
-        return this.price;
-    }
+  public double getPrice() {
+    return this.price;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setPrice(double price) {
-        this.price = price < 0 ? 0.0 : price;
-    }
+  public void setPrice(double price) {
+    this.price = price < 0 ? 0.0 : price;
+  }
 
-    @Override
-    public String toString() {
-        return "Item{" +
-                "id=" + this.id +
-                ", price=" + this.price +
-                ", name='" + this.name + '\'' +
-                "}";
-    }
+  @Override
+  public String toString() {
+    return String.format("Item{id=%d, price=%s, name='%s'}", this.id, this.price, this.name);
+  }
 }
