1143aed77aa6658771b5055445501819ed9c18d2
Agehesha <romashugaev99@gmail.com>
2025-09-23 19:22:29 +0300

    Update Order.java

--- src/main/java/org/example/task2/Order.java
+++ src/main/java/org/example/task2/Order.java
@@ -1,36 +1,32 @@
 package org.example.task2;
 
-
 public class Order {
-    private final long id;
-    private final String customer;
+  private final long id;
+  private final String customer;
 
-    public Order(long id, String customer) {
-        this.id = id;
-        this.customer = customer;
+  public Order(long id, String customer) {
+    this.id = id;
+    this.customer = customer;
+  }
+
+  public String formOrderBill(Cart cart) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Order number ").append(this.id)
+      .append(" for customer ").append(this.customer)
+      .append("\n------------------\n");
+
+    double sum = 0.0;
+    for (Item it : cart.getSnapshot()) {
+      sum += it.getPrice();
+      sb.append("Item id: ").append(it.getId())
+        .append(" name: ").append(it.getName())
+        .append(" price: ").append(it.getPrice())
+        .append("\n");
     }
 
+    sb.append("------------------\n")
+      .append("Total sum: ").append(sum);
 
-    public String formOrderBill(Cart cart) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Order number ").append(this.id)
-               .append(" for customer ").append(this.customer);
-        builder.append("\n------------------\n");
-
-        double sum = 0.0;
-        Item[] items = cart.getSnapshot();
-
-        for (Item it : items) {
-            sum += it.getPrice();
-            builder.append("Item id: ").append(it.getId())
-                   .append(" name: ").append(it.getName())
-                   .append(" price: ").append(it.getPrice())
-                   .append("\n");
-        }
-
-        builder.append("------------------\n");
-        builder.append("Total sum: ").append(sum);
-
-        return builder.toString();
-    }
+    return sb.toString();
+  }
 }
