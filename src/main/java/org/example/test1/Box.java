ac71a171e913f967e9554ba7d1db3b0b481ca44e
Agehesha <romashugaev99@gmail.com>
2025-10-10 15:42:16 +0300

    Update Box.java

--- src/main/java/org/example/test1/Box.java
+++ src/main/java/org/example/test1/Box.java
@@ -2,46 +2,37 @@ public class Box {
 private double length;
 private double width;
 private double height;
-
 public Box(double length, double width, double height) {
 setLength(length);
 setWidth(width);
 setHeight(height);
 }
-  
 public double getLength() { return length; }
 public double getWidth() { return width; }
 public double getHeight() { return height; }
-
 private void setLength(double length) {
 validatePositive("length", length);
 this.length = length;
 }
-
 private void setWidth(double width) {
 validatePositive("width", width);
 this.width = width;
 }
-
 private void setHeight(double height) {
 validatePositive("height", height);
 this.height = height;
 }
-
 private void validatePositive(String name, double value) {
 if (value <= 0) {
 throw new IllegalArgumentException(name + " must be > 0");
 }
 }
-
 public double surfaceArea() {
 return 2.0 * (length * width + length * height + width * height);
 }
-
 public double lateralSurfaceArea() {
 return 2.0 * (length + width) * height;
 }
-
 public double volume() {
 return length * width * height;
 }
