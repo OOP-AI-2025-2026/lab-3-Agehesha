4f479c2149ecc9e9e320d2671efc8ee88b5ba941
Agehesha <romashugaev99@gmail.com>
2025-10-10 15:29:13 +0300

    Update Box.java

--- src/main/java/org/example/test1/Box.java
+++ src/main/java/org/example/test1/Box.java
@@ -1,45 +1,65 @@
-package org.example.test1;
-
 
 public class Box {
-  private double length;
-  private double width;
-  private double height;
 
-  public Box(double length, double width, double height) {
-    setDimension(length, "length");
-    setDimension(width, "width");
-    setDimension(height, "height");
-  }
+private double length;
+private double width;
+private double height;
 
-  private void setDimension(double value, String name) {
-    if (value <= 0) {
-      throw new IllegalArgumentException(name + " must be greater than 0");
-    }
-    switch (name) {
-      case "length":
-        this.length = value;
-        break;
-      case "width":
-        this.width = value;
-        break;
-      case "height":
-        this.height = value;
-        break;
-      default:
-        throw new IllegalArgumentException("Unknown dimension: " + name);
-    }
-  }
 
-  public double getSurfaceArea() {
-    return 2 * (length * width + length * height + width * height);
-  }
+public Box(double length, double width, double height) {
+setLength(length);
+setWidth(width);
+setHeight(height);
+}
 
-  public double getLateralSurfaceArea() {
-    return 2 * height * (length + width);
-  }
 
-  public double getVolume() {
-    return length * width * height;
-  }
+
+public double getLength() { return length; }
+public double getWidth() { return width; }
+public double getHeight() { return height; }
+
+
+
+private void setLength(double length) {
+validatePositive("length", length);
+this.length = length;
+}
+
+
+private void setWidth(double width) {
+validatePositive("width", width);
+this.width = width;
+}
+
+
+private void setHeight(double height) {
+validatePositive("height", height);
+this.height = height;
+}
+
+
+private void validatePositive(String name, double value) {
+if (value <= 0) {
+throw new IllegalArgumentException(name + " must be > 0");
+}
+}
+
+
+
+public double surfaceArea() {
+
+return 2.0 * (length * width + length * height + width * height);
+}
+
+
+public double lateralSurfaceArea() {
+
+return 2.0 * (length + width) * height;
+}
+
+
+public double volume() {
+
+return length * width * height;
+}
 }
