a502950867f2dec94f4184a8d483d727ea637fb6
Agehesha <romashugaev99@gmail.com>
2025-10-11 20:46:45 +0300

    Create Main.java

---
+++ src/main/java/org/example/task1/Main.java
@@ -0,0 +1,12 @@
+package org.example.test1;
+
+public class Main {
+public static void main(String[] args) {
+Box box = new Box(2.5, 1.2, 0.8);
+
+
+System.out.printf("Surface area: %.4f\n", box.surfaceArea());
+System.out.printf("Lateral surface area: %.4f\n", box.lateralSurfaceArea());
+System.out.printf("Volume: %.4f\n", box.volume());
+}
+}
