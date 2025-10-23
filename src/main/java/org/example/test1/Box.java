6000a9fabccd7df002724af47b4c044465c94cab
Agehesha <romashugaev99@gmail.com>
2025-10-23 11:06:59 +0300

    Update Box.java

--- src/main/java/org/example/test1/Box.java
+++ src/main/java/org/example/test1/Box.java
@@ -1,58 +1,33 @@
-package org.example.test1;
+package org.example.task1;
 
 public class Box {
-private double length;
-private double width;
-private double height;
+    private double length;
+    private double width;
+    private double height;
 
+    public Box(double length, double width, double height) {
+        setLength(length);
+        setWidth(width);
+        setHeight(height);
+    }
 
-public Box(double length, double width, double height) {
-setLength(length);
-setWidth(width);
-setHeight(height);
-}
+    private void setLength(double v) { validate(v, "length"); this.length = v; }
+    private void setWidth(double v)  { validate(v, "width");  this.width  = v; }
+    private void setHeight(double v) { validate(v, "height"); this.height = v; }
 
-public double getLength() { return length; }
-public double getWidth() { return width; }
-public double getHeight() { return height; }
+    private static void validate(double v, String name) {
+        if (v <= 0) throw new IllegalArgumentException(name + " must be > 0");
+    }
 
+    public double surfaceArea() {
+        return 2 * (length*width + width*height + length*height);
+    }
 
-private void setLength(double length) {
-validatePositive("length", length);
-this.length = length;
-}
+    public double lateralArea() {
+        return 2 * height * (length + width);
+    }
 
-
-private void setWidth(double width) {
-validatePositive("width", width);
-this.width = width;
-}
-
-
-private void setHeight(double height) {
-validatePositive("height", height);
-this.height = height;
-}
-
-
-private void validatePositive(String name, double value) {
-if (value <= 0) {
-throw new IllegalArgumentException(name + " must be > 0");
-}
-}
-
-
-public double surfaceArea() {
-return 2.0 * (length * width + length * height + width * height);
-}
-
-
-public double lateralSurfaceArea() {
-return 2.0 * (length + width) * height;
-}
-
-
-public double volume() {
-return length * width * height;
-}
+    public double volume() {
+        return length * width * height;
+    }
 }
