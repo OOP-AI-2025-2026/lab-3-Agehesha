e1f8f0ce2afd56f842c4d840145cfe02a803d206
Agehesha <romashugaev99@gmail.com>
2025-10-23 11:09:51 +0300

    Update Main.java

--- src/main/java/org/example/test2/Main.java
+++ src/main/java/org/example/test2/Main.java
@@ -1,31 +1,26 @@
-
-import java.math.BigDecimal;
-
+package org.example.task2;
 
 public class Main {
-public static void main(String[] args) {
-Item apple = new Item("A1", "Apple", new BigDecimal("19.99"));
-Item banana = new Item("B2", "Banana", new BigDecimal("9.49"));
+    public static void main(String[] args) {
+        Cart cart = new Cart(new Item[10]);
+        cart.add(new Item(1, "Samsung Galaxy S23", 27999));
+        cart.add(new Item(2, "Lenovo IdeaPad 3", 19499));
+        cart.add(new Item(3, "LG 55\" 4K Smart TV", 15999));
+        cart.add(new Item(4, "Bosch Serie 6 Пральна машина", 14799));
+        cart.add(new Item(5, "Samsung RB34 Холодильник", 22399));
+        cart.add(new Item(6, "De'Longhi Magnifica S Кавоварка", 10599));
+        cart.add(new Item(7, "Gorenje Електрична плита", 9999));
+        cart.add(new Item(8, "Dyson V11 Пилосос", 16499));
+        cart.add(new Item(9, "Samsung ME83K Мікрохвильова піч", 3199));
+        cart.add(new Item(10, "Philips DryCare Фен", 1499));
 
+        System.out.println(cart);
 
-Cart cart = new Cart();
-cart.add(apple, 2); 
-cart.add(banana, 3);
-cart.setQuantity(banana, 1); 
+        cart.removeById(9L);
 
+        System.out.println(cart);
 
-System.out.println("Cart:");
-for (String line : cart.snapshotView()) System.out.println(" " + line);
-System.out.println("Cart total = " + cart.total());
-
-
-Order order = Order.fromCart("ORD-1001", "Roman Shugaev", cart);
-System.out.println("\nOrder " + order.getOrderId() + " for " + order.getCustomer());
-for (String line : order.viewLines()) System.out.println(" " + line);
-System.out.println("Order total = " + order.total());
-
-
-order.pay();
-System.out.println("Status after pay = " + order.getStatus());
-}
+        Order order = new Order(1L, "John");
+        System.out.println(order.formOrderBill(cart));
+    }
 }
